32da97a2a33b917275ea25024d96f7bea19501b7
Thunderball <vicentkarl@gmail.com>
2021-11-07 00:42:47 +0800

    EDITOR EDITOR

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,17 +1,30 @@
 import "./styles.css";
-import { Button } from "antd";
+import { Button, Row, Col, Form, Modal } from "antd";
 import { useSelector, useDispatch } from "react-redux";
 import { addContainer } from "./store/editor";
 import { Icontainer, Icomponent } from "./model/editor";
 import { RootState } from "../src/store/store";
-// const container;
+import React from "react";
+
+const containerType = {
+  normal: React.createElement("div"),
+  form: Form,
+  dialog: Modal
+};
 export default function App() {
   const containers = useSelector((state: RootState) => {
     return state.editor.containers;
   });
   const dispatch = useDispatch();
 
-  function genContainer(container: Icontainer) {}
+  function genContainer(container: Icontainer) {
+    const Box = containerType[container.containerType];
+    function getComputedStyle(container: Icontainer) {
+      return {};
+    }
+
+    return <Box styles={getComputedStyle}></Box>;
+  }
 
   function genContainers(containers: Icontainer[]) {
     return containers.map((container: Icontainer) => {
